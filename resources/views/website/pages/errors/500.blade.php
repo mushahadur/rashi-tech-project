b14eb1be50d7c1ae8cd2f7425691b7dd41e85746
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2023-12-25 11:44:29 +0600

    error handling done

---
+++ resources/views/website/pages/errors/500.blade.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="{{ asset('/') }}frontend/assets/css/error.css">
+	<title>Error 500 Page</title>
+
+</head>
+<body>
+	
+	<div class="card">
+		<div class="grid">
+			<div class="error">
+				  <span>500
+				   server error
+				  </span>
+			  <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+			  elit  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/>
+			 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+			  ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud 
+			 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<a href="{{ route('home') }}"><button>Go To Home</button></a>
+		 </div>
+	  </div>
+   </div> 
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
